c2abf0873ab75c070c30ce0e58d2d6bf415be09b
sohamotari8944-eng <sohamotari8944@gmail.com>
2026-06-23 16:24:01 +0530

    Add files via upload

---
+++ week-1/funtions.py
@@ -0,0 +1,28 @@
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
+
+# average = (a+b+c)/3
+# print(average)
+ 
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
+
+# average = (a+b+c)/3
+# print(average)
+
+# instead of this use functions and do easily
+
+def avg():
+    a = int(input("Enter your number: "))
+    b = int(input("Enter your number: "))
+    c = int(input("Enter your number: "))
+    
+    average = (a+b+c)/3
+    print(average)
+
+avg()
+avg()
+avg()
+avg()
