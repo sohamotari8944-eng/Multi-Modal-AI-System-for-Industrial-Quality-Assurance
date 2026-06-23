527c75257d8984e39fbf5c698a4dfec7f7286d2c
sohamotari8944-eng <sohamotari8944@gmail.com>
2026-06-23 16:19:17 +0530

    Add files via upload

---
+++ week-1/dict.py
@@ -0,0 +1,9 @@
+marks = {
+    "Harry": 100,
+    "Shubham": 56,
+    "Roham": 23
+}
+
+print(marks, type(marks))
+# print(marks[0])#will give error but...
+print(marks["Harry"])#will give you the mmarks:)
