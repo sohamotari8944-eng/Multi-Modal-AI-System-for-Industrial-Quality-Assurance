d3f810ed49be458c0d52e0ed9d451c21926f9b04
sohamotari8944-eng <sohamotari8944@gmail.com>
2026-06-23 16:10:46 +0530

    Add files via upload

---
+++ week-1/datatypes.py
@@ -0,0 +1,9 @@
+a = 1 # a is an integer
+
+b = 3.33 # b is a floating point integer
+
+c = "Soham" # c is a string
+
+d = False # d is a boolean vairable
+
+e = None # e is a none type variable to tell that there is nothing inn the variable
